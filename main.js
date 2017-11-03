ea68ebc7b2eb46b4a935cd60b044ec66e6475829
DizNicolasAmor <diz.nicolasamor@gmail.com>
2017-11-03 15:12:01 -0200

    Add quiz

--- main.js
+++ main.js
@@ -151,4 +151,143 @@ is displayed along on a large screen, simply do nothing. */
     moveSlider();
   });
   
+
+  //quiz
+  var result = 0,
+      count = 0,
+      questions = [
+        { 
+          q: '1) Who is the main FBI agent in <i>Twin Peaks: Fire Walk with Me</i>?',
+          a: 'Dale Cooper.',
+          option1: 'Fred Madison.',
+          option2: 'Dick Laurent.', 
+          option3: 'Betty Elms.'
+        }, 
+        {
+          q: '2) Which character does Bill Pullman play in <i>Lost Highway</i>?',
+          a: 'Fred Madison.',
+          option1: 'Pete Dayton.',
+          option2: 'Leland.', 
+          option3: 'Jeffrey Beaumont.'
+        },
+        {
+          q: '3) Which instrument does the protagonist of <i>Lost Highway</i> play?',
+          a: 'Saxophone.',
+          option1: 'Piano.',
+          option2: 'Violin.', 
+          option3: 'Guitar.'
+        },
+        {
+          q: '4) Who is the character that has amnesia at the begining of <i>Mulholland Drive</i>?',
+          a: ' Rita Hayworth.',
+          option1: 'Naomi Watts.',
+          option2: 'Laura Palmer.', 
+          option3: 'Betty Elms.'
+        },
+        { 
+          q: '5) Who sees Cooper and the Man from Another Place in a dream in the Black Lodge?',
+          a: 'Laura Palmer.',
+          option1: 'Patricia Arquette.',
+          option2: 'Rita Hayworth.', 
+          option3: 'Dorothy Vallens.'
+        }, 
+        {
+          q: '6) Who composed the music of most of David Lynch movies?',
+          a: 'Angelo Badalamenti.',
+          option1: 'David Lynch.',
+          option2: 'Kyle MacLachlan.', 
+          option3: 'Robert Engels.'
+        },
+        {
+          q: "7) Which is the Lynch's earliest work?",
+          a: 'Eraserhead.',
+          option1: 'Inland Empire.',
+          option2: 'Blue Velvet.', 
+          option3: 'Mulholland Drive.'
+        },
+        {
+          q: "8) Which is the Lynch's latest work?",
+          a: 'Inland Empire.',
+          option1: 'Eraserhead.',
+          option2: 'Mulholland Drive.',
+          option3: 'Lost Highway.'
+        },
+        {
+          q: '9) Where was David Lynch born?',
+          a: 'Montanna.',
+          option1: 'California.',
+          option2: 'London.', 
+          option3: 'New York.'
+        },
+        {
+          q: "10) Which is Dale Cooper's favorite drink?",
+          a: 'Coffee.',
+          option1: 'Beer.',
+          option2: 'Wine.', 
+          option3: 'Water.'
+        }];
+
+  $('#question').html(questions[count].q);
+  $('#option1').html('<input type="radio" name="lynch" value="true">'+questions[count].a);
+  $('#option2').html('<input type="radio" name="lynch" value="false">'+questions[count].option1);
+  $('#option3').html('<input type="radio" name="lynch" value="false">'+questions[count].option2);
+  $('#option4').html('<input type="radio" name="lynch" value="false">'+questions[count].option3);
+
+  $('#submit').click(function(){
+    var selected = $('input:checked').val();
+
+    count++;
+    if(selected === 'true')  result++;
+
+    //reset quiz
+    if( count > 10 ){
+      count = 0;
+      result = 0;
+      $('#submit').html('Submit');
+    }
+
+    //auxiliar function used in next questios
+    var options = [1, 2, 3, 4],
+        shuffledOptions = [];
+
+    Array.prototype.shuffle = function() {
+      var input = this;
+
+      for (var i = input.length-1; i >=0; i--) {
+       
+        var randomIndex = Math.floor(Math.random()*(i+1)); 
+        var itemAtIndex = input[randomIndex]; 
+           
+        input[randomIndex] = input[i]; 
+        input[i] = itemAtIndex;
+      }
+      return input;
+    }
+   
+    //next question
+    if( count < 10){
+      shuffledOptions = options.shuffle();
+
+      $('#question').html(questions[count].q);
+      $('#option' + shuffledOptions[0]).html('<input type="radio" name="lynch" value="true">'+questions[count].a);
+      $('#option' + shuffledOptions[1]).html('<input type="radio" name="lynch" value="false">'+questions[count].option1);
+      $('#option' + shuffledOptions[2]).html('<input type="radio" name="lynch" value="false">'+questions[count].option2);
+      $('#option' + shuffledOptions[3]).html('<input type="radio" name="lynch" value="false">'+questions[count].option3);
+    }
+
+    //show result
+    else if( count = 10){
+      if(result < 4 )  $('#question').html('Your result is: ' + result + '/10 <br><br> You should look more of his movies.');
+      else if(result < 9)  $('#question').html('Your result is: ' + result + '/10 <br><br> Well done!');
+      else  $('#question').html('Your result is: ' + result + '/10 <br><br> You are a Lynchclopedia!');
+
+      $('#option1').html('');
+      $('#option2').html('');
+      $('#option3').html('');
+      $('#option4').html('');
+      $('#submit').html('Play again?');
+    }
+
+    });
+
 });
